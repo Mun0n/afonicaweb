6239bae9017f5a0faa3325ade1a6a20edec8c1d0
mun0n <mun0n@users.noreply.github.com>
2025-01-28 09:37:49 +0100

    Remove Vercel routing attempts

---
+++ public/shop/config/settings.inc.php
@@ -0,0 +1,17 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', 'u597076824_prestashop');
+define('_DB_USER_', 'u597076824_admin_afonica');
+define('_DB_PASSWD_', 'D_jP3Dyyq&R-YyC');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'your_unique_cookie_key_here');
+define('_COOKIE_IV_', 'your_unique_cookie_iv_here');
+define('_PS_CREATION_DATE_', '2024-01-28');
+define('_PS_VERSION_', '8.1.2');
+define('_RIJNDAEL_KEY_', 'your_unique_rijndael_key_here');
+define('_RIJNDAEL_IV_', 'your_unique_rijndael_iv_here');
+define('_PS_DIRECTORY_', 'shop/');
+define('_PS_MODE_DEV_', true); 
