8a1e03139d00e37eca45d09d4f3ed619e9dca56d
mun0n <mun0n@users.noreply.github.com>
2025-02-05 19:01:27 +0100

    fix: update gtag type definition

--- src/types/gtag.d.ts
+++ src/types/gtag.d.ts
@@ -1,15 +1,16 @@
-declare global {
-  interface Window {
-    gtag: (
-      command: 'config' | 'event',
-      targetId: string,
-      config?: {
-        page_path?: string;
-        event_category?: string;
-        event_label?: string;
-        value?: number;
-        [key: string]: any;
-      }
-    ) => void;
-  }
-} 
+interface Window {
+  gtag: (
+    command: 'config' | 'event',
+    targetId: string,
+    config?: {
+      page_path?: string;
+      event_category?: string;
+      event_label?: string;
+      value?: number;
+      [key: string]: any;
+    }
+  ) => void;
+  dataLayer: any[];
+}
+
+export {}; 
