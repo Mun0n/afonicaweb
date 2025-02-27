1a6f87c5a09dfb36563835376b6756cb2ad22fe3
mun0n <mun0n@users.noreply.github.com>
2025-02-27 18:16:51 +0100

    fix: update vercel config to skip type checking

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const withBundleAnalyzer = bundleAnalyzer({
 });
 
 const config = {
+  typescript: {
+    ignoreBuildErrors: process.env.SKIP_TYPE_CHECK === 'true',
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
