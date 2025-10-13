8d579edfe520afbc962ec6d4949899b4560aa42e
mun0n <mun0n@users.noreply.github.com>
2025-10-13 09:19:54 +0200

    Add MAKE ANDY GREAT AGAIN t-shirt product with correct images

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -39,6 +39,17 @@ export const mockBandInfo: BandInfo = {
         back: '/images/products/tshirt-chayanne-back.webp'
       },
       prestashopUrl: 'https://shop.afonicanaranjo.com/merch-afonica-naranjo/2-12-aprobado-por-chayanne-camiseta-afonica-naranjo.html#/3-talla-unisex_l'
+    },
+    {
+      id: 'tshirt-andy-lucas',
+      name: 'MAKE ANDY GREAT AGAIN – Camiseta Afónica Naranjo edición Andy & Lucas',
+      description: 'Edición especial de la camiseta Afónica Naranjo con el diseño Andy & Lucas, inspirada en la dualidad del ser y el humor eterno de la banda. Color azul eléctrico con impresión frontal del logo y reverso con diseño minimalista.',
+      price: 18,
+      images: {
+        front: '/images/products/t-shirt-andy-front.webp',
+        back: '/images/products/t-shirt-andy-back.webp'
+      },
+      prestashopUrl: 'https://shop.afonicanaranjo.com/merch-afonica-naranjo/3-19-make-andy-great-again-camiseta-afonica-naranjo.html#/1-talla-unisex_xxl'
     }
   ],
   socialMedia: [
